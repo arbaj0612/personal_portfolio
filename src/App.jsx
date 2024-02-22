c0324c1992639ebe07bc09b897dab205701b8c9b
arbaj0612 <junaid.786mulani@gmail.com>
2024-02-22 23:09:46 +0530

    Project finished

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,9 @@ import React from 'react'
 import Navbar from './Components/Navbar'
 import Home from './Components/Home'
 import About from './Components/About'
+import Skills from './Components/Skills'
+import Work from './Components/Work'
+import Contact from './Components/Contact'
 
 
 function App() {
@@ -10,6 +13,9 @@ function App() {
         <Navbar/>
         <Home/>
         <About/>
+        <Skills/>
+        <Work/>
+        <Contact/>
     </div>
   )
 } 
--- src/Components/Home.jsx
+++ src/Components/Home.jsx
@@ -8,7 +8,7 @@ const Home = () => {
             <div className='max-w-[1000px] mx-auto px-8 flex flex-col justify-center h-full text-gray-300'>
                 <p className='text-[#1D9CA8]'>Hi, my name is </p>
                 <h1 className='text-4xl sm:text-7xl font-bold text-[#ccd6f6]'>Arbaj Mulani</h1>
-                <h2 className='text-4xl sm:text-7xl font-bold text-[#8892b0]'>I'm a Front End Developer.</h2>
+                <h2 className='text-4xl sm:text-7xl font-bold text-[#8892b0]'>I&apos;m a Front End Developer.</h2>
                 <p className='text-[#8892b0] py-4 max-w-[700px]'>
                     I'm a passionate front-end developer dedicated to crafting
                     seamless  and engaging web experiences,
